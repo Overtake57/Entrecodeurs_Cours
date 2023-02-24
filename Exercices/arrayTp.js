24db7b735e7bc45d30a6780cd89b7ff252c3837d
Overtake57 <agnes.yoann@gmail.com>
2023-02-24 16:24:27 +0100

    Update arrayTp.js

--- Exercices/arrayTp.js
+++ Exercices/arrayTp.js
@@ -3,12 +3,12 @@ const student = {
   gender: "male",
   js: {
     mark: 12,
-    rate: 3
+    rate: 3,
   },
   english: {
     mark: 4,
-    rate: 2
-  }
+    rate: 2,
+  },
 };
 
 const dw1 = [
@@ -17,61 +17,61 @@ const dw1 = [
     gender: "male",
     js: {
       mark: 12,
-      rate: 3
+      rate: 3,
     },
     english: {
       mark: 7,
-      rate: 2
-    }
+      rate: 2,
+    },
   },
   {
     firstName: "Marc",
     gender: "male",
     js: {
       mark: 9,
-      rate: 3
+      rate: 3,
     },
     english: {
       mark: 19,
-      rate: 2
-    }
+      rate: 2,
+    },
   },
   {
     firstName: "Amar",
     gender: "male",
     js: {
       mark: 16,
-      rate: 3
+      rate: 3,
     },
     english: {
       mark: 2,
-      rate: 2
-    }
+      rate: 2,
+    },
   },
   {
     firstName: "Oscar",
     gender: "male",
     js: {
       mark: 16,
-      rate: 3
+      rate: 3,
     },
     english: {
       mark: 12,
-      rate: 2
-    }
+      rate: 2,
+    },
   },
   {
     firstName: "Juliette",
     gender: "female",
     js: {
       mark: 10,
-      rate: 3
+      rate: 3,
     },
     english: {
       mark: 11,
-      rate: 2
-    }
-  }
+      rate: 2,
+    },
+  },
 ];
 const dw2 = [
   {
@@ -79,78 +79,80 @@ const dw2 = [
     gender: "male",
     js: {
       mark: 12,
-      rate: 3
+      rate: 3,
     },
     english: {
       mark: 4,
-      rate: 2
-    }
+      rate: 2,
+    },
   },
   {
     firstName: "Baptiste",
     gender: "male",
     js: {
       mark: 12,
-      rate: 3
+      rate: 3,
     },
     english: {
       mark: 9,
-      rate: 2
-    }
+      rate: 2,
+    },
   },
   {
     firstName: "Hélène",
     gender: "female",
     js: {
       mark: 13,
-      rate: 3
+      rate: 3,
     },
     english: {
       mark: 20,
-      rate: 2
-    }
+      rate: 2,
+    },
   },
   {
     firstName: "Paola",
     gender: "female",
     js: {
       mark: 12,
-      rate: 3
+      rate: 3,
     },
     english: {
       mark: 7,
-      rate: 2
-    }
-  }
+      rate: 2,
+    },
+  },
 ];
 
-// Cette deuxieme partie concerne les tableaux, attention ces tableaux contiennent des objets. 
+// Cette deuxieme partie concerne les tableaux, attention ces tableaux contiennent des objets.
 // Vous pourrez utiliser les boucles for afin de traiter chaque element des tableaux
-// Une autre solution que la boucle for sont les fonctions "map" et "forEach" utilisable sur un tableau (voir doc cdn) 
-
+// Une autre solution que la boucle for sont les fonctions "map" et "forEach" utilisable sur un tableau (voir doc cdn)
 
 // 1. Combien y a t il d'élève dans la classe dw1 puis dw2 et dans toutes les classes ?
 
-console.log('Il y a ' + dw1.length + ' eleves dans la classe dw1 et ' + dw2.length + ' eleves dans la classe dw2');
+console.log(
+  "Il y a " +
+    dw1.length +
+    " eleves dans la classe dw1 et " +
+    dw2.length +
+    " eleves dans la classe dw2"
+);
 console.log("il y a donc " + (dw1.length + dw2.length) + " eleves present");
-
 
 // 2. Lister l'ensemble des élèves des deux classes dans l'ordre alphabétique
 
 const students = dw1.concat(dw2);
-console.log(students.sort((a, b) => a.firstName.localeCompare(b.firstName)))
-
+console.log(students.sort((a, b) => a.firstName.localeCompare(b.firstName)));
 
 // 3. Prendre les 3 premiers élèves de la classe dw1
 
-const firstOfDw1 = dw1.slice(0,3)
-console.log(firstOfDw1)
-
+const firstOfDw1 = dw1.slice(0, 3);
+console.log(firstOfDw1);
 
 // 4. Prendre le dernier élève de la classe dw1
 
-const lastOfDw1 = dw1.slice(-1)
-console.log(lastOfDw1)
+const lastOfDw1 = dw1.slice(-1);
+console.log(lastOfDw1);
 
 // 5. Moyenne générale de chaque élève
 
@@ -179,36 +181,79 @@ console.log(getStudentsNotesAverage(dw2));
 // 7. Moyenne des deux classes en js ?
 
 // 8. Meilleure moyenne de la classe en js ?
-const moyenneJsOfDw1 =dw1.sort((a,b) => {
+const moyenneJsOfDw1 = dw1.sort((a, b) => {
   return a.js.mark > b.js.mark ? 1 : -1;
 });
 console.log(moyenneJsOfDw1[0].firstName, moyenneJsOfDw1[0].js.mark);
 
 // 9. Meilleur élève des deux classes
-const toutesMesClasses = dw2.concat(dw1).sort((a,b) => {
+const toutesMesClasses = dw2.concat(dw1).sort((a, b) => {
   return a.js.mark < b.js.mark ? 1 : -1;
-})
-console.log(
-  toutesMesClasses[0].firstName,
-  toutesMesClasses[0].js.mark
-)
+});
+console.log(toutesMesClasses[0].firstName, toutesMesClasses[0].js.mark);
 // 10. Meilleure fille des deux classes en anglais
 console.log(
   dw2
-  .concat(dw1)
-  .filter(student => student.gender === 'female')
-  .sort ((a, b) => a.english.mark > b.english.mark ? 1 : -1)[0]
+    .concat(dw1)
+    .filter((student) => student.gender === "female")
+    .sort((a, b) => (a.english.mark > b.english.mark ? 1 : -1))[0]
 );
 
 // 11. Meilleur garcon en moyenne générale
+const garcons = dw1
+  .filter((student) => student.gender === "male")
+  .map((student) => {
+    const jsMark = student.js.mark;
+    const jsRate = student.js.rate;
+    const englishMark = student.english.mark;
+    const englishRate = student.english.rate;
+    const moyenneGenerale =
+      (jsMark * jsRate + englishMark * englishRate) / (jsRate + englishRate);
+    return {
+      firstName: student.firstName,
+      moyenneGenerale: moyenneGenerale,
+    };
+  });
+
+const filles = dw1
+  .filter((student) => student.gender === "female")
+  .map((student) => {
+    const jsMark = student.js.mark;
+    const jsRate = student.js.rate;
+    const englishMark = student.english.mark;
+    const englishRate = student.english.rate;
+    const moyenneGenerale =
+      (jsMark * jsRate + englishMark * englishRate) / (jsRate + englishRate);
+    return {
+      firstName: student.firstName,
+      moyenneGenerale: moyenneGenerale,
+    };
+  });
+
+garcons.sort((a, b) => b.moyenneGenerale - a.moyenneGenerale);
+const meilleurGarcon = garcons[0];
 console.log(
-  dw2
-  .concat(dw1)
-  .filter(student => student.gender === 'male')
-  .sort ((a, b) => a.english.mark > b.english.mark ? 1 : -1)[0]
+  meilleurGarcon.firstName +
+    " est le meilleur garçon avec une moyenne générale de " +
+    meilleurGarcon.moyenneGenerale.toFixed(2) +
+    "."
 );
 
 // 12. Qui a la note médiane en anglais parmi dw1 ?
+const englishMarks = dw1.map((student) => student.english.mark);
+englishMarks.sort((a, b) => a - b);
+const middle = Math.floor(englishMarks.length / 2);
+const medianEnglishMark =
+  englishMarks.length % 2 === 0
+    ? (englishMarks[middle - 1] + englishMarks[middle]) / 2
+    : englishMarks[middle];
+const studentWithMedianEnglishMark = dw1.find(
+  (student) => student.english.mark === medianEnglishMark
+);
+console.log(
+  studentWithMedianEnglishMark.firstName +
+    " a la note médiane en anglais dans dw1."
+);
 
 // 12.a. Donner sa position dans la classe
 
